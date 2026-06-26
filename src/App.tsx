d7f9e04f3e30381d7f6af81aaa2836aaedf97508
worlasie-gf <worlasie@gridforge.energy>
2026-06-26 16:27:12 -0700

    Set router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gridforge-vision-83">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}

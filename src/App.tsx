bf6b15c548fb198ffab9c389f23e4db855d7881c
worlasie-gf <worlasie@gridforge.energy>
2026-06-26 17:06:42 -0700

    Use root router path for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/gridforge-vision-83">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
